1f96e1c35bfae58466204bc5ac1e4a0a3e5af624
luisfernando <luis_oliveira@usp.br>
2018-06-26 06:59:57 -0300

    Tem muitos erros, corrige isso ai

--- inserecarta.c
+++ inserecarta.c
@@ -2,6 +2,7 @@
 
 void inserecarta(carta ** Vetores, int v, carta ** Grupos){
 	int c;
+	int f=0;
 	int g;
 	int r;
 	int i;
@@ -15,23 +16,23 @@ void inserecarta(carta ** Vetores, int v, carta ** Grupos){
 	scanf("%d",&r);
 
 	if(r==0){
-		printf("Digite o indice da carta a ser insirida: ");
+		printf("Digite o indice da carta a ser inserida: ");
 		scanf("%d",&c);
 		c--;
-		printf("Digite o indice do grupo em que a carta sera insirida: ");
+		printf("Digite o indice do grupo em que a carta sera inserida: ");
 		scanf("%d",&g);
 		g--;
 
 		if(Vetores[v][c].valor==(*(Grupos[g])).valor){
 			carta * Percorre = Grupos[g];
 			while(Percorre!=NULL){
-				if(*Percorre.naipe == '@'){
+				if(Percorre->naipe == '@'){
 					B[0] = 1;
 				}else{
-					if(*(Percorre).naipe == '!'){
+					if(Percorre->naipe == '!'){
 						B[1] = 1;
 					}else{
-						if(*(Percorre).naipe == '$'){
+						if(Percorre->naipe == '$'){
 							B[2] = 1;
 						}else{
 							B[3] = 1;
@@ -69,7 +70,6 @@ void inserecarta(carta ** Vetores, int v, carta ** Grupos){
 			printf("A carta nao possui o mesmo valor\n");
 			return;
 		}
-
 		for(i=0;i<50;i++){
 			if(Vetores[0][i].valor==0){
 				Vetores[0][i] = Vetores[v][c];
@@ -84,6 +84,64 @@ void inserecarta(carta ** Vetores, int v, carta ** Grupos){
 				Percorre->prox = &Vetores[0][i];
 				break;
 			}
+		}
+	}else{
+		if(r==1){
+			carta * Percorre;
+			printf("Digite o indice da carta a ser inserida: ");
+			scanf("%d",&c);
+			c--;
+			printf("Digite o indice da sequencia em que a carta sera inserida: ");
+			scanf("%d",&g);
+			g--;
+			if(Grupos[g]->valor=='*'){
+				if(Vetores[v][c].valor==Grupos[g]->valor-2){
+					f = 1;
+				}
+			}else{
+				if(Vetores[v][c].valor==Grupos[g]->valor-1){
+					f = 2;
+				}else{
+					Percorre = Grupos[g];
+					while(Percorre->prox!=NULL){
+						Percorre = Percorre->prox;
+					}
+					if(*(Percorre).valor=='*'){
+						if(Vetores[v][c].valor==*(Percorre).valor+2){
+							f = 3;
+						}
+					}else{
+						if(Vetores[v][c].valor==*(Percorre).valor+1){
+							f = 4;
+						}
+					}
+				}
+			}
+			if(f==0){
+				printf("Carta escolhida nao eh valida\n");
+				return;
+			}
+			for(i=0;i<50;i++){
+				if(Vetores[0][i].valor==0){
+					Vetores[0][i] = Vetores[v][c];
+					Vetores[v][c].valor = 0;
+					Vetores[v][c].naipe = 0;
+					if(f==1 || f==2){
+						Vetores[0][i].prox = Grupos[g];
+						Grupos[g] = &Vetores[0][i];
+					}else{
+						Percorre = Grupos[g];
+						while(Percorre->prox!=NULL){
+							Percorre = Percorre->prox;
+						}
+						Percorre->prox = &Vetores[0][i];
+					}
+					break;
+				}
+			}
+		}else{
+			printf("Valor Invalido\n");
+			return;
 		}
 	}
 }
